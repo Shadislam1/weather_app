4f6b6e7e15fbe1d40177f4dd441956a234eb24c3
shad <shadislam999@gmail.com>
2023-12-13 13:07:23 +0600

    update

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -7,7 +7,14 @@ class WeatherScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
-        title: const Text('Weather App') ,
+        title: const Text('Weather App',style: TextStyle(
+          fontWeight: FontWeight.bold,
+        ) 
+        ) ,
+        centerTitle: true,
+        actions:  [
+       IconButton(onPressed: (){}, icon: const Icon(Icons.refresh),),
+        ],
       ),
     );
   }
